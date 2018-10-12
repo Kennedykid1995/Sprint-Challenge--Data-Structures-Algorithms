a27f0e7e98cc04c9069551714c1b711e2f9ec197
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-12 11:36:55 -0500

    Depth_first_for_each
    Only figured this out by finding something online.

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,9 +5,23 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    #create an empty stack
+    stack=[]
+    #append the self to the stack
+    stack.append(self)
+    if stack > 0:
+      #create a object that holds the values within stack
+      current = stack.pop()
+      #give the callback function current.
+      cb(current.value)
+      #find out whether the right or left side has any values, if so then append the current.value right or left.
+      if current.right is not None:
+        stack.append(current.right)
+      if current.left is not None:
+        stack.append(current.left)
 
   def breadth_first_for_each(self, cb):
+    
     pass
 
   def insert(self, value):
